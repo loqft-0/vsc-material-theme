c84518a31d50231a3a8b1c710367138ceef8d379
Jeff Hykin <jeff.hykin@gmail.com>
2019-12-11 12:20:22 -0600

    -

---
+++ .info_language_runner_cache.rb
@@ -0,0 +1,19 @@
+require 'atk_toolbox'
+
+system "_ build"
+previous_process_finished_successfully = $?.success?
+if previous_process_finished_successfully
+    # push build
+    if Console.args[0]
+        system "project sync --message='#{Console.args[0]}'"
+    else
+        system "project sync"
+    end
+    
+    if -"npm version patch"
+        system "project synchronize --message='version bump'"
+        puts "Publishing"
+        system "vsce publish"
+        puts "Finished Publishing"
+    end
+end
